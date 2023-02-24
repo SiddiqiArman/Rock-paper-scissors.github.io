0b490fccd937eb73cbb35aa1e3adccd64827a0c4
ARMAN SIDDIQI <siddiqiarman926@gmail.com>
2023-02-25 04:51:07 +0530

    final version of basic program

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+let count1;
+let count2;
+count1 =0;
+count2 =0;
 function getComputerChoice(){
     let randomNumber =Math.floor(Math.random()*10)+1;
     if( randomNumber<4){
-        return "rock":
+        return "rock";
     }
     else if(randomNumber>4&&randomNumber<8){
         return "paper";
@@ -10,35 +14,66 @@ function getComputerChoice(){
         return "scissor";
     }
 }
+
 function playRound(computerChoice,userChoice){
     if (userChoice==="PAPER"&&computerChoice==="ROCK"){
-        return "You won/n paper beats rock";
+        count1++;
+        return "You won ! paper beats rock";
     }
     else if (userChoice==="ROCK"&&computerChoice==="PAPER"){
-        return "You lose paper beats rock";
+        count2++;
+        return "You lose ! paper beats rock";
     }
     else if (userChoice==="ROCK"&&computerChoice==="SCISSOR"){
-        return "You won rock beats scissor";
+        count1++;
+        return "You won ! rock beats scissor";
     }
     else if (userChoice==="SCISSOR"&&computerChoice==="ROCK"){
-        return "You lose rock beats scissor";
+        count2++;
+        return "You lose ! rock beats scissor";
     }
     else if (userChoice==="SCISSOR"&&computerChoice==="PAPER"){
-        return "You won scissor beats paper";
+        count1++;
+        return "You won ! scissor beats paper";
     }
     else if (userChoice==="PAPER"&&computerChoice==="SCISSOR"){
-        return "You lose scissor beats paper";
+        count2++;
+        return "You lose ! scissor beats paper";
     }
     else {
         return "match tie";
     }
 
 }
-let computerChoice =getComputerChoice();
-computerChoice =computerChoice.toUpperCase();
-let userChoice = prompt('enter your choice');
-userChoice =userChoice.toUpperCase();
-// console.log
+function game(){
+    let computerChoice;
+    let userChoice;
+    for(let i=0;i<5;i++){
+        computerChoice =getComputerChoice();
+        computerChoice =computerChoice.toUpperCase();
+        userChoice = prompt('enter your choice');
+        userChoice =userChoice.toUpperCase();
+        console.log(playRound(computerChoice,userChoice));
+    }
+   
+    if(count1>count2){
+        console.log("computer score : "+count2);
+        console.log("user score : "+count1);
+        console.log("YOU WON");
+    }
+    else{
+        console.log("computer score : "+count2);
+        console.log("user score : "+count1);
+        console.log("YOU LOSE");
+    }
 
-
-
+}
+// let computerChoice =getComputerChoice();
+// computerChoice =computerChoice.toUpperCase();
+// let userChoice = prompt('enter your choice');
+// userChoice =userChoice.toUpperCase();
+// // console.log
+// console.log("the computer choice is  "+computerChoice)
+// console.log("the user choice is  "+userChoice)
+// console.log(playRound(computerChoice,userChoice))
+game()
